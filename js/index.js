048aac7d2c3b81b510e397c6d25b3903c99944a9
Ryan Lin <43688324+ryanlin@users.noreply.github.com>
2018-11-11 09:20:46 -0800

    Beginning Files
    Add folder for css with style.css
    Add folder for js with index.js
    Add main page (index.html)
    Add Registration page (registration.html)

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+// learn with this JQuery Guide https://www.digitalocean.com/community/tutorials/an-introduction-to-jquery
+
+$(document).ready(function() {
+	$('.header').height($(window).height());
+
+})
+
+$(document).ready(function() {
+  $('.overlay').height($(window).height());
+
+})
